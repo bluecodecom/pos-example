fc5a21de2a8d1b4c5861e1ae3ed3fcbfe2fd44d4
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2018-06-04 10:35:43 +0300

    Adding http to https redirect

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { setCredentials } from './credentials-components';
 
+/** The CORS calls don't work on http so redirect to https if trying to access an insecure domain. */
+function redirectToHttps() {
+  const PRODUCTION_DOMAIN = "pos-example.bluecode.com"
+  
+  if (document.location.protocol === "http:" 
+      && document.location.host === PRODUCTION_DOMAIN) {
+    document.location = "https://" + PRODUCTION_DOMAIN
+  }
+}
+
 let element = document.getElementById('insert-js-pos-here')
 
 if (element) {
+  redirectToHttps()
+
   let username = element.getAttribute('username')
   let password = element.getAttribute('password')
   let branch = element.getAttribute('branch')
