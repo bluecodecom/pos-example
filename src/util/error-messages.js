207ead3b344c1d6f003f943c4a0e110da2241aa6
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2018-10-29 07:18:08 -0600

    Error code for invalid branch was wrong

--- src/util/error-messages.js
+++ src/util/error-messages.js
@@ -24,7 +24,7 @@ export const ERROR_SYSTEM_FAILURE = 'SYSTEM_FAILURE'
 export const ERROR_UNAVAILABLE = 'SERVICE_UNAVAILABLE' // this is a 503 status code
 export const ERROR_ISSUER_NOT_SUPPORTED = 'ISSUER_NOT_SUPPORTED'
 export const ERROR_INVALID_BARCODE = 'INVALID_BARCODE'
-export const ERROR_INVALID_BRANCH = 'INVALID_BRANCH'
+export const ERROR_BRANCH_NOT_FOUND = 'BRANCH_NOT_FOUND'
 export const ERROR_INVALID_PARAMETER = 'ERROR_INVALID_PARAMETER'
 export const ERROR_FRAUD_DETECTED = 'FRAUD_DETECTED'
 export const ERROR_INSUFFICIENT_FUNDS = 'INSUFFICIENT_FUNDS'
@@ -48,7 +48,7 @@ export const MESSAGES = {
     [ERROR_MERCHANT_TX_ID_NOT_UNIQUE]: 'Internal error ("transaction ID not unique"). Please try again. Contact support if the problem persists.',
     [ERROR_ISSUER_NOT_SUPPORTED]: 'The customer\'s Blue Code account is not supported.',
     [ERROR_INVALID_BARCODE]: 'Invalid barcode. Ask the customer to generate a new code and try again.',
-    [ERROR_INVALID_BRANCH]: 'Wrong configuration (invalid branch ID). Please contact an administrator.',
+    [ERROR_BRANCH_NOT_FOUND]: 'Wrong configuration (invalid branch ID). Please contact an administrator.',
     [ERROR_FRAUD_DETECTED]: 'Payment was declined. Use a different form of payment.',
     [ERROR_INSUFFICIENT_FUNDS]: 'Payment was declined. Use a different form of payment.',
     [ERROR_INVALID_STATE]: 'Payment was declined. Use a different form of payment.',
@@ -73,7 +73,7 @@ export const MESSAGES = {
     [ERROR_MERCHANT_TX_ID_NOT_UNIQUE]: 'Technischer Fehler ("transaction ID not unique"). Bitte versuchen Sie es erneut. Kontaktieren Sie die Service-Hotline wenn der Fehler besteht.',
     [ERROR_ISSUER_NOT_SUPPORTED]: 'Dieses Blue Code-Konto wird leider nicht unterstützt.',
     [ERROR_INVALID_BARCODE]: 'Falscher Strichcode. Bitten Sie den Kunden einen neuen Code zu generieren und versuchen Sie es erneut.',
-    [ERROR_INVALID_BRANCH]: 'Falsche Kassenkonfiguration (Blue Code Branch ID ist ungültig). Bitte einen Administrator kontaktieren.',
+    [ERROR_BRANCH_NOT_FOUND]: 'Falsche Kassenkonfiguration (Blue Code Branch ID ist ungültig). Bitte einen Administrator kontaktieren.',
     [ERROR_FRAUD_DETECTED]: 'Zahlung wurde abgelehnt. Bitte anderes Zahlungsmittel benutzen.',
     [ERROR_INSUFFICIENT_FUNDS]: 'Zahlung wurde abgelehnt. Bitte anderes Zahlungsmittel benutzen.',
     [ERROR_INVALID_STATE]: 'Zahlung wurde abgelehnt. Bitte anderes Zahlungsmittel benutzen.',
